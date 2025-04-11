aae8fbc9f67d6bce5103685c91f85390be7a42f0
Aleksi Kantonen <150439194+aleksikantonen@users.noreply.github.com>
2025-04-11 10:40:46 +0300

    part 4 demoing

---
+++ tests/reverse.test.js
@@ -0,0 +1,24 @@
+const { test, describe } = require('node:test')
+const assert = require('node:assert')
+
+const reverse = require('../utils/for_testing').reverse
+
+describe('reverse', () => {
+  test('reverse of a', () => {
+    assert.strictEqual(reverse('a'), 'a')
+  })
+
+  // test('reverse of react', () => {
+  //   const result = reverse('react')
+
+  //   assert.strictEqual(result, 'tcaer')
+  // })
+
+  test('reverse of react', () => {
+    assert.strictEqual(reverse('react'), 'tkaer')
+  })
+
+  test('reverse of saippuakauppias', () => {
+    assert.strictEqual(reverse('saippuakauppias'), 'saippuakauppias')
+  })
+})
